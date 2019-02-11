d165ea572b6ff041b73162de74738bd62f5b3f49
kawa1228 <sxa.1105bo@gmail.com>
2019-02-11 16:56:17 +0900

    feat: cookiesの追加

--- pages/authed-route.vue
+++ pages/authed-route.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
     <h2>認証が必要なページ</h2>
+    <p>Cokkieの 'credential' の値が Truthy な場合のみアクセス可能です。</p>
   </div>
 </template>
--- pages/login.vue
+++ pages/login.vue
@@ -1,5 +1,20 @@
 <template>
   <div>
     <h2>ログインページ</h2>
+    <button　type="button" @click="login"> ログイン </button>
   </div>
 </template>
+
+<script>
+import Cookies from 'universal-cookie'
+
+export default {
+  methods: {
+    login() {
+      const cookies = new Cookies()
+      cookies.set('credential', 'true', { maxAge: 90 })
+      this.$router.push('/')
+    }
+  }
+}
+</script>
